73eeafcd625a4ef845d88c929477c5dddd8faf77
chocomelo24 <mansoorchanique@gmail.com>
2022-08-22 08:16:29 +0200

    Second commit

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -12,4 +12,94 @@ router.get("/", (req, res) => {
     res.status(400).send(error);
   }
 });
+
+router.get("/:id", (req, res) => {
+  try {
+    con.query(
+      `SELECT * FROM products where product_id =${req.params.id}`,
+      (err, result) => {
+        if (err) throw err;
+        res.send(result);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post("/", (req, res) => {
+  if (req.res) {
+    const {
+      sku,
+      name,
+      price,
+      weight,
+      descriptions,
+      thumbnail,
+      image,
+      category,
+      create_date,
+      stock,
+    } = req.body;
+
+    // Insert product
+    try {
+      con.query(
+        `INSERT into products (	sku,	name,	price,	weight, descriptions,	thumbnail,	image,	category,	create_date, stock) 
+      values ('${sku}', '${name}', '${price}', '${weight}', '${descriptions}', '${thumbnail}', '${image}', '${category}' , '${create_date}', '${stock}')`,
+        (err, result) => {
+          if (err) throw err;
+          res.send(result);
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    res.send("Access Denied");
+  }
+});
+
+// edit product
+router.put("/:id", (req, res) => {
+  const {
+    sku,
+    name,
+    price,
+    weight,
+    descriptions,
+    thumbnail,
+    image,
+    category,
+    create_date,
+    stock,
+  } = req.body;
+  try {
+    con.query(
+      `UPDATE products SET sku="${sku}",name="${name}",price="${price}",weight="${weight}",descriptions="${descriptions}",thumbnail="${thumbnail}",image="${image}",category="${category}", create_date="${create_date}" ,stock="${stock}"  WHERE product_id="${req.params.id}"`,
+      (err, result) => {
+        if (err) throw err;
+        res.send(result);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// delete products
+router.delete("/:id", (req, res) => {
+  try {
+    con.query(
+      `DELETE FROM products  WHERE product_id="${req.params.id}"`,
+      (err, result) => {
+        if (err) throw err;
+        res.send(result);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 module.exports = router;
