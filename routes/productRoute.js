6b38cd7a0496941a11bf562a1482ce5ebca852bf
chocomelo24 <mansoorchanique@gmail.com>
2022-08-31 13:24:20 +0200

    try3

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -48,7 +48,7 @@ router.post("/", (req, res) => {
     try {
       con.query(
         `INSERT into products (	sku,	name,	price,	size, descriptions,	thumbnail,	image,	category,	create_date, stock) 
-      values ('${sku}', '${name}', '${price}', '${size}', '${descriptions}', '${thumbnail}', '${image}', '${category}' , '${create_date}', '${stock}')`,
+      values ('${sku}', '${name}', '${price}', '${size}', '${descriptions}', '${thumbnail}', '${image}', '${category}', '${create_date}', '${stock}')`,
         (err, result) => {
           if (err) throw err;
           res.send(result);
@@ -78,7 +78,7 @@ router.put("/:id", (req, res) => {
   } = req.body;
   try {
     con.query(
-      `UPDATE products SET sku="${sku}" ,name="${name}" ,price="${price}" ,size="${size}"  ,descriptions="${descriptions} ,thumbnail="${thumbnail}" ,image="${image}" ,category="${category}", create_date="${create_date}" ,stock="${stock}"  WHERE product_id="${req.params.id}"`,
+      `UPDATE products SET sku="${sku}",name="${name}",price="${price}",size="${size}",descriptions="${descriptions}",thumbnail="${thumbnail}",image="${image}",category="${category}", create_date="${create_date}" ,stock="${stock}"  WHERE product_id="${req.params.id}"`,
       (err, result) => {
         if (err) throw err;
         res.send(result);
